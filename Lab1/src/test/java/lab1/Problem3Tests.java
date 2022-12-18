145755e4c2e4eb06338af8ec8da77553293f0a0d
velikiyurod <99759669+LevchukKirill@users.noreply.github.com>
2022-12-18 23:59:02 +0200

    Update Problem3Tests.java

--- Lab1/src/test/java/lab1/Problem3Tests.java
+++ Lab1/src/test/java/lab1/Problem3Tests.java
@@ -32,12 +32,14 @@ public class Problem3Tests {
     void flattenMatrix_inputShape2x5_expectedOutputShape10() {
         // given
         int[][] inputArray = {
-                { 0, 1, 2, 3, 4 },
-                { 5, 6, 7, 8, 9 }
+                { 2, 4, 5, 4 },
+                { 6, 9, 8, 3 },
+                { 6, 7, 1, 0 },
+                { 1, 6, 5, 1 }
         };
 
         // expected
-        int[] expectedOutputArray = { 0, 5, 1, 6, 2, 7, 3, 8, 4, 9 };
+        int[] expectedOutputArray = { 2, 4, 5, 4, 6, 9, 8, 3, 6, 7, 1, 0, 1, 6, 5, 1 };
 
         // result
         assertTrue(Arrays.equals(
