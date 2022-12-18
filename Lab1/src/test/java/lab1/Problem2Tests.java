e3707a35664ad1fb876e19476fcb47f169b96551
velikiyurod <99759669+LevchukKirill@users.noreply.github.com>
2022-12-19 00:18:28 +0200

    Update Problem2Tests.java

--- Lab1/src/test/java/lab1/Problem2Tests.java
+++ Lab1/src/test/java/lab1/Problem2Tests.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Problem2Tests {
 
     @Test
-    void segregateEvenAndOddNumbers_input21568_expectedOutput26815() {
+    void segregateEvenAndOddNumbers_test1() {
         // given
         int[] inputArray = { 2, 1, 5, 6, 8 };
 
-        // expected
+        // when
         int[] expectedOutputArray = { 2, 6, 8, 1, 5 };
 
-        // result
+        // then
         assertTrue(Arrays.equals(
                 Problem2.segregateEvenAndOddNumbers(inputArray),
                 expectedOutputArray
@@ -25,33 +25,17 @@ public class Problem2Tests {
     }
 
     @Test
-    void segregateEvenAndOddNumbers_input84602_expectedOutput84602() {
+    void segregateEvenAndOddNumbers_test2() {
         // given
-        int[] inputArray = { 8, 4, 6, 0, 2 };
+        int[] inputArray = { 1, 2, 3, 4, 5, 6 };
 
-        // expected
-        int[] expectedOutputArray = { 8, 4, 6, 0, 2 };
+        // when
+        int[] expectedOutputArray = { 2, 4, 6, 1, 3, 5 };
 
-        // result
+        // then
         assertTrue(Arrays.equals(
                 Problem2.segregateEvenAndOddNumbers(inputArray),
                 expectedOutputArray
         ));
     }
-
-    @Test
-    void segregateEvenAndOddNumbers_input97351_expectedOutput97351() {
-        // given
-        int[] inputArray = { 9, 7, 3, 5, 1 };
-
-        // expected
-        int[] expectedOutputArray = { 9, 7, 3, 5, 1 };
-
-        // result
-        assertTrue(Arrays.equals(
-                Problem2.segregateEvenAndOddNumbers(inputArray),
-                expectedOutputArray
-        ));
-    }
-
 }
